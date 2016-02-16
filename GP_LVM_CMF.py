94bc6a94e44cfd0ec06be18149a9a25dd80ccfb4
roryjbeard <roryjbeard@gmail.com>
2016-02-16 00:09:46 +0000

    didn't quite get the bug last time

--- GP_LVM_CMF.py
+++ GP_LVM_CMF.py
@@ -146,7 +146,7 @@ class SGPDV(object):
         if th.config.floatX == 'float32':
            # Need a lot more jitter for 32 bit computation
            self.jitterDefault = np.float32(0.1)
-        else
+        else:
             self.jitterDefault = np.float32(0.0001)
         self.jitterGrowthFactor = np.float32(1.1)
         self.jitter = th.shared(np.asarray(self.jitterDefault, dtype=precision), name='jitter')
