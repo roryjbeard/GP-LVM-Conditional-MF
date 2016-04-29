bafaba35be6d3653d8f92642937bdac5f3d0d045
Chris Lloyd <lloyd.cm@gmail.com>
2016-04-29 17:57:08 +0100

    Minor bugs fixed

--- parmesan_dev/utils_RB.py
+++ parmesan_dev/utils_RB.py
@@ -1,8 +1,8 @@
 import math
 import theano.tensor as T
-c = - 0.5 * math.log(2*math.pi)
+c = math.log(2*math.pi)
 
 def normalEntropy2(log_var):
     '''elementwise entropy of a fully factorized normal'''
-    H = (0.5 - c) + log_var
+    H = 0.5 * (c + 1 + log_var)
     return H
